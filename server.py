aa230b239c60c4e59bf419fad2788326e4fe47e6
Noppanit Charassinvichai <noppanit.c@gmail.com>
2015-09-06 18:31:05 -0400

    Added Port

--- server.py
+++ server.py
@@ -27,4 +27,5 @@ def find():
     return json.dumps(results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
